a51938aafda93649c038f70efd28bc2d9354ee8c
ulyan0v <73800859+ulyan0v@users.noreply.github.com>
2020-12-01 14:07:24 +0300

    change modal window component

--- src/components/modalWindow/index.jsx
+++ src/components/modalWindow/index.jsx
@@ -3,10 +3,20 @@ import "./modalWindow.scss";
 
 export class ModalWindow extends React.Component {
   render() {
+    const { open, title, onClose, children } = this.props;
+
+    if (!open) {
+      return null;
+    }
+
     return (
       <div className="modal-window">
         <div className="modal-window-dialog">
-          {this.props.children}
+          <div className="modal-window-header">
+            <h3>{title}</h3>
+            <button onClick={onClose}>&times;</button>
+          </div>
+          {children}
         </div>
       </div>
     );
